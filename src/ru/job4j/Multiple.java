8fe8203e91030b6fe0e0947233736be0d076cd82
Artem <artem.polschak@gmail.com>
2020-09-19 16:47:21 +0200

    Разбор первой программы. [#367492]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            int a = 1;
+            int res = a * i;
+            System.out.println(res);
+
+        }
+    }
+}
