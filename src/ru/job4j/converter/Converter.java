f4600573e92b8132781c0f3a4e77c4f4b58c82ee
Artem <artem.polschak@gmail.com>
2020-09-20 20:51:22 +0200

    5. Конвертер валюты [#367472]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,18 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
+        return rsl;
+    }
+
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+    }
+}
