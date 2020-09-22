186aaadc4c516bbb8e6e1c1d2a05bef6ab97cf9a
Artem <artem.polschak@gmail.com>
2020-09-22 16:50:15 +0200

    9.1. String [#367483]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -1,14 +1,13 @@
 package ru.job4j.condition;
 
 public class Greeting {
+
     public static void main(String[] args) {
         String idea = "I like Java!";
-        int year = 2020;
         System.out.println(idea);
         System.out.println(idea + " But I am a newbie");
+        int year = 2020;
         System.out.println(idea + year);
-
-
 
     }
 }
