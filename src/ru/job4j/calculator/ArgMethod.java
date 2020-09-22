4e2d521c21feb513875f1743397ea0807e619b76
Artem <artem.polschak@gmail.com>
2020-09-22 02:25:56 +0200

    9.2. Checkstyle [#367488]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -2,8 +2,9 @@ package ru.job4j.calculator;
 
 public class ArgMethod {
 
-    public static void hello(String name){
-         System.out.println("Hello " + name );
+    public static void hello(String name) {
+
+        System.out.println("Hello " + name);
      }
 
     public static void main(String[] args) {
