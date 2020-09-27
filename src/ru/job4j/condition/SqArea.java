5aeaef7ca5720a5556e02e1a7a1b1b709f309619
Artem <artem.polschak@gmail.com>
2020-09-27 14:17:48 +0200

    4.6.2. Switch - matcher [#367431]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,12 +1,12 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
+    public static int square(int p, int k) {
         //
-        double h = p / (2 * (k + 1));
-        double l = h * k;
-        double rsl = l * h;
-        return rsl;
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 
     public static void main(String[] args) {
--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -1,18 +1,27 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SqAreaTest {
 
     @Test
-    public void whenP6K2then2() {
-        int expected = 2;
-        int p = 6;
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
         int k = 2;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        int p = 6;
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 }
