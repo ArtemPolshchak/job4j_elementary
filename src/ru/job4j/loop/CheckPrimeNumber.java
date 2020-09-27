2ad1fb05b30c732eb890c1d02e1d41948f3fd806
Artem <artem.polschak@gmail.com>
2020-09-28 01:48:50 +0200

    5.5. Простое число [#367532]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,10 +1,19 @@
 package ru.job4j.loop;
 
+import static java.lang.StrictMath.sqrt;
+
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-        if ((number % number) == 0) {
-            prime = true;
+        boolean prime = true;
+        if (number < 2) {
+            prime = false;
+        }
+
+        for (int i = 1; i <= number; i++) {
+             if ((number % 2) == 0) {
+                prime = false;
+                break;
+            }
         }
         return prime;
     }
--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -16,12 +16,12 @@ public class CheckPrimeNumberTest {
     @Test
     public void when4() {
         boolean rsl = CheckPrimeNumber.check(4);
-        assertThat(rsl, is(true));
+        assertThat(rsl, is(false));
     }
 
     @Test
     public void when1() {
         boolean rsl = CheckPrimeNumber.check(1);
-        assertThat(rsl, is(true));
+        assertThat(rsl, is(false));
     }
 }
