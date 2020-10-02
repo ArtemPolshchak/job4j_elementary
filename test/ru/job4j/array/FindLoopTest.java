0cfa86b460e7e4930951e59ae8a3f80e45aef8a3
Artem <artem.polschak@gmail.com>
2020-10-02 21:31:58 +0200

    6.1. Классический поиск перебором. [#367439]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class FindLoopTest {
+
     @Test
     public void whenArrayHas5Then0() {
         int input[] = {5, 4, 3, 2};
@@ -14,6 +15,8 @@ public class FindLoopTest {
         int expect = 0;
         assertThat(result, is(expect));
     }
+
+    @SuppressWarnings("checkstyle:ArrayTypeStyle")
     @Test
     public void whenArrayHas3Then2() {
         int input[] = {5, 4, 7, 2};
