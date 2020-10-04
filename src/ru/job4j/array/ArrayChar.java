85ed484cbd0c3ae833da076e3cf094f5245021f1
Artem <artem.polschak@gmail.com>
2020-10-04 18:00:03 +0200

    6.4. Слово начинается с ... [#367442]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -7,6 +7,7 @@ public class ArrayChar {
         for (int index = 0; index < pref.length; index++) {
             if (pref[index] != word[index]) {
                 result = false;
+                break;
             }
         }
         return result;
