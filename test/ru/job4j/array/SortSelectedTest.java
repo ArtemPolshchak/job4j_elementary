64050ae64cd46785574ef3e15834376701f51963
Artem <artem.polschak@gmail.com>
2020-10-08 22:19:38 +0200

    6.5.3. Сортировка выборкой. [#367448]

--- test/ru/job4j/array/SortSelectedTest.java
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -22,4 +22,11 @@ public class SortSelectedTest {
         int[] expect = new int[] {1, 2, 3};
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenSort8() {
+        int[] input = new int[] {90, 20, 10, 50, 100, 60, 30, 1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 10, 20, 30, 50, 60, 90, 100};
+        assertThat(result, is(expect));
+    }
 }
