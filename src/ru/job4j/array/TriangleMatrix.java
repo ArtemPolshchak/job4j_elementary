7154133eae05667eae8b161afbac0de237677c80
Artem <artem.polschak@gmail.com>
2020-12-07 19:11:54 +0200

    exceptions

--- src/ru/job4j/array/TriangleMatrix.java
+++ src/ru/job4j/array/TriangleMatrix.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 import java.util.Arrays;
 
 public class TriangleMatrix {
-
     public static int[][] rows(int count) {
 
         int[][] triangle = new int[count][];
@@ -11,7 +10,7 @@ public class TriangleMatrix {
         int number2 = 1;
 
         for (int i = 0; i < triangle.length; i++) {
-            triangle[i] = new int[number];
+           triangle[i] = new int[number];
 
             for (int j = 0; j < triangle[i].length; j++) {
                 triangle[i][j] = number2++;
@@ -19,5 +18,10 @@ public class TriangleMatrix {
             number++;
         }
         return triangle;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(rows(4)) + System.lineSeparator());
     }
 }
