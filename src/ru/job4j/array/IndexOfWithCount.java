16a54fa00af2a484ac5afc3ca4333098b16c1f30
Artem <artem.polschak@gmail.com>
2020-12-15 01:00:21 +0200

    21. indexOf с числом вхождений

---
+++ src/ru/job4j/array/IndexOfWithCount.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class IndexOfWithCount {
+    public static int indexOf(char[] string, char c, int number) { //abcd
+        int rsl = -1;
+        char[] temp = new char[c];
+        for (int index = 0; index < string.length; index++) { //0(a) < 4
+            if (string[index] == temp[])
+        }
+        return rsl;
+    }
+}
