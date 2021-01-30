f430909acb5069b78cce1e28399bbf658eb3fc79
Artem <artem.polschak@gmail.com>
2021-01-30 18:28:04 +0200

    22. Максимальная длина неубывающей последовательности

--- src/ru/job4j/array/MaxLengthSeria.java
+++ src/ru/job4j/array/MaxLengthSeria.java
@@ -3,10 +3,35 @@ package ru.job4j.array;
 public class MaxLengthSeria {
     public static int find(int[] array) {
         int count = 0;
+        int res = 0;
+
+        int tmp = 1;
+        int[] tmpCounts = new int[array.length];
+
         for (int index = 0; index < array.length; index++) {
-            if (array[index] < )
+
+            if (array[index] >= tmp) {
+                tmp = array[index];
+                count++;
+                res = count;
+            } else {
+                tmpCounts[index] = count;
+                count = 0;
+            }
+
         }
-        return count;
+        for (int i = 0; i < tmpCounts.length - 1; i++) {
+            if (tmpCounts[i] >= tmpCounts[i + 1]) {
+                res = tmpCounts[i];
+            }
+        }
+        return res;
+
     }
 
+    public static void main(String[] args) {
+        System.out.println(find(new int[] {2, 3, 4, 5}));
+
+    }
 }
+
