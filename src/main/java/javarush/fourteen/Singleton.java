c4c562f5c2b203f5ba04b60c648a831cbb31ead5
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2021-11-16 14:46:00 +0200

    Singleton

---
+++ src/main/java/javarush/fourteen/Singleton.java
@@ -0,0 +1,17 @@
+package javarush.fourteen;
+
+
+public class Singleton {
+    private static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    private Singleton() {
+
+    }
+}
