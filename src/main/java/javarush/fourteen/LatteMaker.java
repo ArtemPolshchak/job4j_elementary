3fc3870323709f51f7fd22507d9afda4ae1c92da
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2021-11-18 23:34:08 +0200

    java

---
+++ src/main/java/javarush/fourteen/LatteMaker.java
@@ -0,0 +1,20 @@
+package javarush.fourteen;
+
+
+public class LatteMaker extends DrinkMaker{
+    @Override
+    void getRightCup() {
+        System.out.println("Берем чашку для латте");
+
+    }
+
+    @Override
+    void putIngredient() {
+        System.out.println("Делаем молоко с пенкой");
+    }
+
+    @Override
+    void pour() {
+        System.out.println("Заливаем кофе");
+    }
+}
