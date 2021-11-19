70a9da7f8db88c6e9d6a1733bed9a849a5c91bf4
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2021-11-19 03:30:58 +0200

    java

---
+++ src/main/java/javarush/fourteen/DifferentTypesForMethods.java
@@ -0,0 +1,47 @@
+package javarush.fourteen;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Разные методы для разных типов
+*/
+
+public class DifferentTypesForMethods {
+    public static void main(String[] args) throws IOException {
+        //напиште тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while (!(str = reader.readLine()).equals("exit")) {
+        try {
+            if (str.contains(".")) {
+                print(Double.parseDouble(str));
+            } else if (Integer.parseInt(str) <= 0 || Integer.parseInt(str) >= 128) {
+                    print(Integer.parseInt(str));
+            } else if (Short.parseShort(str) > 0 && Short.parseShort(str) < 128) {
+                print(Short.parseShort(str));
+            }
+        } catch (Exception e) {
+            print(str);
+        }
+        }
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
+    }
+}
