982392d6f200239171622ca04be0e7a0a04cc23b
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2021-11-20 02:54:18 +0200

    java
    1

---
+++ src/main/java/javarush/fourteen/Solution.java
@@ -0,0 +1,25 @@
+package javarush.fourteen;
+
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
+class Statics {
+    public static String FILE_NAME = "D:/1.txt"/* add the path to your source file here */;
+}
