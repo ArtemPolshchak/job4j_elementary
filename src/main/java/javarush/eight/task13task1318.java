61df0573f271d343c1d0f86d588c2528c335f956
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-01-01 17:56:16 +0200

    javarush

--- src/main/java/javarush/eight/task13task1318.java
+++ src/main/java/javarush/eight/task13task1318.java
@@ -1,10 +1,9 @@
-package javarush.eightLvl;
+package javarush.eight;
 
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /*
 Чтение файла
