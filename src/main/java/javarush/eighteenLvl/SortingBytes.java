e6c55a3fce409fc0757c7f7569cdddeb88c7a32c
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-01-31 00:05:32 +0200

    Сортировка байтов Java Core

--- src/main/java/javarush/eighteenLvl/SortingBytes.java
+++ src/main/java/javarush/eighteenLvl/SortingBytes.java
@@ -1,31 +1,45 @@
 package javarush.eighteenLvl;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /**
  * @author Artem Polshchak on 28.01.2022.
  * @project javaRush Задачи на FileInputStream, FileOutputStream
+ * Сортировка байт
  * Java Core
  * 8 уровень, 3 лекция
  * Ввести с консоли имя файла.
- * Найти максимальный байт в файле, вывести его на экран.
+ * Считать все байты из файла.
+ * Не учитывая повторений - отсортировать их по байт-коду в возрастающем порядке.
+ * Вывести на экран.
  * Закрыть поток ввода-вывода.
+ *
+ * Пример байт входного файла:
+ * 44 83 44
+ *
+ * Пример вывода:
+ * 44 83
  */
-public class MaxByte {
+public class SortingBytes {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String dataFile = reader.readLine();
         FileInputStream inputStream = new FileInputStream(dataFile);
-        int res = 0;
+        Set<Integer> set = new TreeSet<>();
+
 
         while (inputStream.available() > 0) {
             int data = inputStream.read();
-            if (data >= res) {
-                res = data;
-            }
+            set.add(data);
         }
         inputStream.close();
-        System.out.println(res);
 
+        for (Integer s : set) {
+            System.out.print(s + " ");
+        }
     }
 }
