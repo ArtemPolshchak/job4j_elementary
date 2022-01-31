2594d3fa586204442909a812fe70b7752878d502
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-01 01:43:44 +0200

    Java Core

---
+++ src/main/java/javarush/eighteenLvl/CountComma.java
@@ -0,0 +1,33 @@
+package javarush.eighteenLvl;
+
+import java.io.*;
+
+/**
+ * @author Artem Polshchak on 31.01.2022.
+ * @project javaRush
+ * Подсчет запятых
+ * Java Core
+ * 8 уровень, 5 лекция
+ * С консоли считать имя файла.
+ * Посчитать в файле количество символов ',', количество вывести на консоль.
+ * Закрыть потоки.
+ *
+ * Подсказка:
+ * нужно сравнивать с ascii-кодом символа ','.
+ */
+public class CountComma {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream = new FileInputStream(reader.readLine());
+
+        int res = 0;
+        while (inputStream.available() > 0) {
+            if (inputStream.read() == ',') {
+                res++;
+            }
+        }
+        inputStream.close();
+        System.out.println(res);
+    }
+}
+
