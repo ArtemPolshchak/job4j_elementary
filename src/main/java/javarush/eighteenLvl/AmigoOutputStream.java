1d0139f395467e28a5712fd942da8c0ad76325dd
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-03 00:15:25 +0200

    Java Core

---
+++ src/main/java/javarush/eighteenLvl/AmigoOutputStream.java
@@ -0,0 +1,14 @@
+package javarush.eighteenLvl;
+
+import java.io.IOException;
+public interface AmigoOutputStream {
+    void flush() throws IOException;
+
+    void write(int b) throws IOException;
+
+    void write(byte[] b) throws IOException;
+
+    void write(byte[] b, int off, int len) throws IOException;
+
+    void close() throws IOException;
+}
