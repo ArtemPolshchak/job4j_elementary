587a3c92205d34a8325b0ad7c97c85dbde7f9c12
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-02-08 03:33:13 +0200

    JavaRush

---
+++ src/main/java/javarush/eighteenLvl/AmigoOutStream.java
@@ -0,0 +1,16 @@
+package javarush.eighteenLvl;
+
+import java.io.IOException;
+
+public interface AmigoOutStream {
+
+    void flush() throws IOException;
+
+    void write(int b) throws IOException;
+
+    void write(byte[] b) throws IOException;
+
+    void write(byte[] b, int off, int len) throws IOException;
+
+    void close() throws IOException;
+}
--- src/main/java/javarush/eighteenLvl/AmigoOutputStream.java
+++ src/main/java/javarush/eighteenLvl/AmigoOutputStream.java
@@ -1,14 +1,60 @@
 package javarush.eighteenLvl;
 
-import java.io.IOException;
-public interface AmigoOutputStream {
-    void flush() throws IOException;
+import java.io.*;
 
-    void write(int b) throws IOException;
+/**
+ * @author Artem Polshchak on 01.02.2022.
+ * @project javaRush
+ * AmigoOutputStream
+ * Java Core
+ * 8 уровень, 8 лекция
+ * 1 Измени класс AmigoOutputStream так, чтобы он стал Wrapper-ом для класса FileOutputStream. Используй наследование.
+ * 2 При вызове метода close() должны выполняться следующая последовательность действий:
+ * 2.1 Вызвать метод flush().
+ * 2.2 Записать в конец файла фразу "JavaRush © All rights reserved.", используй метод getBytes().
+ * 2.3 Закрыть поток методом close().
+ */
 
-    void write(byte[] b) throws IOException;
+public class AmigoOutputStream extends FileOutputStream {
+    public static String fileName = "C:/tmp/result.txt";
+    FileOutputStream fileOutputStream;
 
-    void write(byte[] b, int off, int len) throws IOException;
+    public AmigoOutputStream(FileOutputStream name) throws FileNotFoundException {
+        super(String.valueOf(name));
+    }
 
-    void close() throws IOException;
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        fileOutputStream.write(b);
+
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        fileOutputStream.write(b);
+
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fileOutputStream.write(b, off, len);
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.flush();
+        fileOutputStream.write( "JavaRush © All rights reserved.".getBytes());
+        fileOutputStream.close();
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new AmigoOutputStream(new FileOutputStream(fileName));
+    }
 }
