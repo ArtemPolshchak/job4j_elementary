9e334be877256f996affbc2c036e4ba3a69c5814
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-12-18 04:01:22 +0200

    java

---
+++ src/main/java/ru/job4j/condition/StringRunner.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+/**
+ * @author artem.polschak@gmail.com on 18.12.2022.
+ * @project job4j_elementary
+ */
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        int year = 2022;
+        idea += year;
+        System.out.println(idea);
+    }
+}
